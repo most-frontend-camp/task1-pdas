81ec5cf61acf3ff75079a7b878836d55f5a15431
Beibarys Learner <beibarys.learner@gmail.com>
2024-04-28 23:50:41 +0500

    used bulma and updated app.js and profilecard.js

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,13 +1,30 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
     return (
         <div>
             <div>Personal Digital Assistants</div>
 
-            <ProfileCard title = "Alexa " handle = "@alexa2022" />
-            <ProfileCard title = "Cortana " handle = "@cortana2023" />
-            <ProfileCard title = "Siri " handle = "@siri2024" />
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-3">
+                            <ProfileCard tatle="Alexa " handle="@alexa2022" image={AlexaImage}/>
+                        </div>
+                        <div className="column is-3">
+                            <ProfileCard title="Cortana " handle="@cortana2023" image={CortanaImage}/>
+                        </div>
+                        <div className="column is-3">
+                            <ProfileCard title="Siri " handle="@siri2024" image={SiriImage}/>
+                        </div>
+                    </div>
+                </section>
+            </div>
+
         </div>
     );
 }
--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,8 +1,20 @@
-function ProfileCard({ title, handle} ) {
+function ProfileCard({ title, handle, image} ) {
+    console.log(title, handle);
     return (
-      <div>
-          <div>Title is {title}</div>
-          <div>Handle is {handle}</div>
+      <div className="card">
+          <div className="card-image">
+              <figure className="image is-1by1">
+                  <img src={image} alt="pda logo"/>
+              </figure>
+          </div>
+
+        <div className="card-content">
+            <div className="media-content">
+                <p className="title is-4">{title}</p>
+                <p className="subtitle is-6">{handle}</p>
+            </div>
+        </div>
+
       </div>
     );
 
